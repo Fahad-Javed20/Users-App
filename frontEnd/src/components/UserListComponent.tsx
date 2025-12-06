668f19f674be7cf3ca04bf69151263efd787a1d9
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2025-12-07 01:16:09 +0500

    Update UserListComponent.tsx

--- frontEnd/src/components/UserListComponent.tsx
+++ frontEnd/src/components/UserListComponent.tsx
@@ -6,23 +6,24 @@ interface UserListComponentProps {
 
 const UserListComponent = ({ usersList }: UserListComponentProps    ) => {
   return (
-    <div>
-        <table>
-            <thead>
+    <div >
+        {/* want to decrese space between table celss */}
+        <table className="w-full ">
+            <thead className="bg-gray-200">
                 <tr>
-                    <th className="border px-4 py-2">Name</th>
-                    <th className="border px-4 py-2">Email</th>
-                    <th className="border px-4 py-2">City</th>
-                    <th className="border px-4 py-2">Salary</th>
+                    <th className="border px-4 py-1">Name</th>
+                    <th className="border px-4 py-1">Email</th>
+                    <th className="border px-4 py-1">City</th>
+                    <th className="border px-4 py-1">Salary</th>
                 </tr>
             </thead>
             <tbody>
                 {usersList.map((user) => (
                     <tr key={user.id}>
-                        <td className="border px-4 py-2">{user.name}</td>
-                        <td className="border px-4 py-2">{user.email}</td>
-                        <td className="border px-4 py-2">{user.city}</td>
-                        <td className="border px-4 py-2">{user.salary}</td>
+                        <td className="border px-4 py-1">{user.name}</td>
+                        <td className="border px-4 py-1">{user.email}</td>
+                        <td className="border px-4 py-1">{user.city}</td>
+                        <td className="border px-4 py-1">{user.salary}</td>
                     </tr>
                 ))}
             </tbody>
